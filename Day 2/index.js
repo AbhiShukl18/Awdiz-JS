e06c406f2a43d36ef69573ff1435f274ec1b78f4
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-04-18 14:03:53 +0530

    Day 2

---
+++ Day 2/index.js
@@ -0,0 +1,36 @@
+var firstnum=50
+var secondnum=50
+
+console.log(firstnum+secondnum)
+console.log(firstnum-secondnum)
+console.log(firstnum*secondnum)
+console.log(firstnum/secondnum)
+console.log(firstnum%secondnum)
+
+console.log(1, typeof(1))
+console.log("hello", typeof("hello"))
+console.log(true, typeof(true))
+console.log(false, typeof(false))
+console.log(50+50)
+console.log("50"+50)
+
+let x=10 // declare
+x= 15 // re-assign possible
+// let x= 12 // re-declare not possible, it will throw error
+
+const pi=3.14 //declare
+// pi=4 // re-asssign not possible, it will throw error
+// const pi=5 // re-declare not possible, it will throw error
+
+console.log(name)
+var name="Abhi"
+console.log(name)
+name="Abhis"
+console.log(name)
+var name="Abhishek"
+console.log(name)
+
+console.log(typeof("Abhi"))
+console.log(typeof(1))
+console.log(typeof(true))
+
