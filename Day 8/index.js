536c6bb7ac01ae9c3c9ffbf5c676a2bc8ebf46f6
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-04-28 22:03:29 +0530

    Day 8

---
+++ Day 8/index.js
@@ -0,0 +1,27 @@
+// Q. Print reverse array, using for loop
+
+// var array=[1,2,8,10];
+// for(i=0; i<= array.length-1; i++){
+
+//     console.log(array[i]);
+// }
+
+// for(i=array.length-1; i>=0; i--){
+
+//     console.log(array[i]);
+// }
+
+// Q. Add nos in array which are odd
+ var num= [1,4,2,9,80,99];
+ var count=0;
+ for(i=0;i<=num.length;i++){
+
+    if(num[i]%2===1){
+
+        count+=num[i];
+    }
+ }
+
+ console.log(count);
+
+ //Q, Find name of students whose name is greater than 4
